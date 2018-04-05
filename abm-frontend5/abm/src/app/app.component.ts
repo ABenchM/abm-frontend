6624170c698207fd0423fcc79a88358678262851
Ankur Gupta <agupta@mail.uni-paderborn.de>
2018-04-05 18:26:52 +0200

    Initial changes

--- abm-frontend5/abm/src/app/app.component.ts
+++ abm-frontend5/abm/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'abm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  pageTitle = 'Automated Benchmark Management';
 }
