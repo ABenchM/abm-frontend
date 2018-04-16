1fe9ec116eb1e5a97d1683d4b83fd4edb133bc4b
Hariharan <hariharansrc@gmail.com>
2018-04-16 15:17:22 +0200

    Adapting index.js to run both old and new angular  applications.

--- index-old.js
+++ index-old.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const proxy = require('http-proxy-middleware');
-app.use(express.static('abm-frontend5/abm/dist'))
+app.use(express.static('abm-frontend-old'))
 app.use('/rest', proxy({target: 'https://abm.cs.upb.de/', changeOrigin: true}));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))  
