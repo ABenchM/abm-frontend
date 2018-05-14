26e21c719e8b44d4c97cc37d05e5d511c6538e7e
Hariharan Ramanathan <rhari@mail.uni-paderborn.de>
2018-05-14 08:52:19 +0200

    Config docker

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 git clone https://github.com/ABenchM/abm-frontend
 cd abm-frontend
+git checkout develop
 npm install
 cd abm-frontend5/abm
 npm install
-npm install  @angular/cli
+npm install -g  @angular/cli
