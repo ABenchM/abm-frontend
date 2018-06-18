93c1a19768fb1f7cbdcde2e257d1dd8b0303df51
Chinmay <cppingale@gmail.com>
2018-06-18 19:16:22 +0200

    ts checkstyle fixes
    ts checkstyle fixes

--- abm-frontend5/abm/src/app/services/register.service.ts
+++ abm-frontend5/abm/src/app/services/register.service.ts
@@ -2,8 +2,8 @@ import {Injectable} from '@angular/core';
 import {Http , Response, Headers, RequestOptions} from '@angular/http';
 import {User} from '../models/user.model';
 import { IfObservable } from 'rxjs/observable/IfObservable';
-import 'rxjs/Rx';
-import {Observable} from 'rxjs/Observable';
+import {Observable} from 'rxjs/Rx';
+import 'rxjs/Observable';
 @Injectable()
 export class Register {
     static invalidUsername: boolean;
