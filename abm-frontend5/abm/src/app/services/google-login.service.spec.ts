e7530615796d20d4f368217a74ebbc1ef208295c
Ankur Gupta <agupta@mail.uni-paderborn.de>
2018-09-17 13:20:59 +0200

    Unit test cases for Services

--- abm-frontend5/abm/src/app/services/google-login.service.spec.ts
+++ abm-frontend5/abm/src/app/services/google-login.service.spec.ts
@@ -1,14 +1,18 @@
 import { TestBed, inject } from '@angular/core/testing';
 import { GoogleLoginService } from './google-login.service';
+import { Http, HttpModule} from '@angular/http';
+import { AuthService, SocialLoginModule, AuthServiceConfig } from 'angular-6-social-login';
+import { getAuthServiceConfigs } from '../SocialLoginConfig';
 
 describe('GoogleLoginService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [GoogleLoginService]
+      imports: [HttpModule, SocialLoginModule],
+      providers: [GoogleLoginService , SocialLoginModule, AuthService , {provide: AuthServiceConfig, useFactory: getAuthServiceConfigs }]
     });
   });
 
-  it('should be created', inject([GoogleLoginService], (service: GoogleLoginService) => {
+  fit('should be created', inject([GoogleLoginService], (service: GoogleLoginService) => {
     expect(service).toBeTruthy();
   }));
 });
