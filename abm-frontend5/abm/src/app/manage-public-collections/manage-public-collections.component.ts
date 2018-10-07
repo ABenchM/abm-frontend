3db7ff0f3e104e95be4c7c3fdf69d371457b3f62
TalalE <vishaljcj@gmail.com>
2018-10-07 19:19:29 +0200

    Create method that call rest API to delete collection. Delete public collection by clicking on the icons in frontend

--- abm-frontend5/abm/src/app/manage-public-collections/manage-public-collections.component.ts
+++ abm-frontend5/abm/src/app/manage-public-collections/manage-public-collections.component.ts
@@ -37,41 +37,12 @@ export class ManagePublicCollectionsComponent implements OnInit {
     this.dataSource.sort = this.sort;
   }
 
-  
-
   applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
-}
+  deletePublicCollection(coll: Collection ){
+    this.service.deletePublicCollection(coll).subscribe();
+  }
 
-/*
-export interface Element {
-  id: string;
-  name: string;
-  description: string;
-  owner: string;
-  creationDate:string;
-  status:string;
 }
-
-const ELEMENT_DATA: Element[] = [
-  { id: "894cd- 5desc1 1223445", name: 'Java', description: "Java is a general-purpose computer-programming language that is concurrent class-based Java is a general-purpose computer-programming language that is concurrent class-based Java is a general-purpose computer-programming language that is concurrent class-based", owner: 'Vishal', creationDate: "14/02/2018",status:"Active"},
-  { id: "894cd- 5desc1 1223445", name: 'Machine learning', description: "Machine learning is a subset of artificial intelligence in the field", owner: 'Menal', creationDate: "12/01/108",status:"Active" },
-  { id: "894cd- 5desc1 1223445", name: 'Angular', description: "Angular is a TypeScript-based open-source front-end web application platform led by the Angular Team", owner: 'Talal', creationDate: "12/01/108",status:"Inctive" },
-  { id: "894cd- 5desc1 1223445", name: 'Java', description: "Java is a general-purpose computer-programming language that is concurrent class-based", owner: 'Vishal', creationDate: "14/02/2018",status:"Active"},
-  { id: "894cd- 5desc1 1223445", name: 'Machine learning', description: "Machine learning is a subset of artificial intelligence in the field", owner: 'Menal', creationDate: "12/01/108",status:"Active" },
-  { id: "894cd- 5desc1 1223445", name: 'Angular', description: "Angular is a TypeScript-based open-source front-end web application platform led by the Angular Team", owner: 'Talal', creationDate: "12/01/108",status:"Inctive" },
-  { id: "894cd- 5desc1 1223445", name: 'Java', description: "Java is a general-purpose computer-programming language that is concurrent class-based", owner: 'Vishal', creationDate: "14/02/2018",status:"Active"},
-  { id: "894cd- 5desc1 1223445", name: 'Machine learning', description: "Machine learning is a subset of artificial intelligence in the field", owner: 'Menal', creationDate: "12/01/108",status:"Active" },
-  { id: "894cd- 5desc1 1223445", name: 'Angular', description: "Angular is a TypeScript-based open-source front-end web application platform led by the Angular Team", owner: 'Talal', creationDate: "12/01/108",status:"Inctive" },
-  { id: "894cd- 5desc1 1223445", name: 'Java', description: "Java is a general-purpose computer-programming language that is concurrent class-based", owner: 'Vishal', creationDate: "14/02/2018",status:"Active"},
-  { id: "894cd- 5desc1 1223445", name: 'Machine learning', description: "Machine learning is a subset of artificial intelligence in the field", owner: 'Menal', creationDate: "12/01/108",status:"Active" },
-  { id: "894cd- 5desc1 1223445", name: 'Angular', description: "Angular is a TypeScript-based open-source front-end web application platform led by the Angular Team", owner: 'Talal', creationDate: "12/01/108",status:"Inctive" },
-  { id: "894cd- 5desc1 1223445", name: 'Java', description: "Java is a general-purpose computer-programming language that is concurrent class-based", owner: 'Vishal', creationDate: "14/02/2018",status:"Active"},
-  { id: "894cd- 5desc1 1223445", name: 'Machine learning', description: "Machine learning is a subset of artificial intelligence in the field", owner: 'Menal', creationDate: "12/01/108",status:"Active" },
-  { id: "894cd- 5desc1 1223445", name: 'Angular', description: "Angular is a TypeScript-based open-source front-end web application platform led by the Angular Team", owner: 'Talal', creationDate: "12/01/108",status:"Inctive" },
-  { id: "894cd- 5desc1 1223445", name: 'Java', description: "Java is a general-purpose computer-programming language that is concurrent class-based", owner: 'Vishal', creationDate: "14/02/2018",status:"Active"},
-  { id: "894cd- 5desc1 1223445", name: 'Machine learning', description: "Machine learning is a subset of artificial intelligence in the field", owner: 'Menal', creationDate: "12/01/108",status:"Active" },
-  { id: "894cd- 5desc1 1223445", name: 'Angular', description: "Angular is a TypeScript-based open-source front-end web application platform led by the Angular Team", owner: 'Talal', creationDate: "12/01/108",status:"Inctive" },
-];*/
