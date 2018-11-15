e26c7e3de0f9724283ff2bd3b3537d858aa59381
mac24079 <mac24079@mail.uni-paderborn.de>
2018-11-15 08:16:11 +0100

    resolved failing of bs-navbar test case

--- abm-frontend5/abm/src/app/bs-navbar/bs-navbar.component.spec.ts
+++ abm-frontend5/abm/src/app/bs-navbar/bs-navbar.component.spec.ts
@@ -4,11 +4,7 @@ import { BsNavbarComponent } from './bs-navbar.component';
 import { CapitalizeFirstPipe } from '../shared/capitalize-first.pipe';
 import { HttpModule } from '@angular/http';
 import { CurrentUserService } from '../services/current-user.service';
-<<<<<<< HEAD
 import { MatButtonModule, MatMenuModule, MatIconModule, MatTableModule, MatPaginatorModule, MatFormFieldModule, MatInputModule, MatSortModule, MatDialogModule, MatCheckboxModule } from '@angular/material';
-=======
-import { MatMenuModule, MatIconModule} from '@angular/material';
->>>>>>> d6e6d31e63443a79c49bc034de744089938234e9
 
 describe('BsNavbarComponent', () => {
   let component: BsNavbarComponent;
