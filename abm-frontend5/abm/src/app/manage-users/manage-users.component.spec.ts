c84ba36dbc81ab47a047552d43436c5c044796d7
TalalE <vishaljcj@gmail.com>
2018-11-15 10:47:54 +0100

    test cases for user service

--- abm-frontend5/abm/src/app/manage-users/manage-users.component.spec.ts
+++ abm-frontend5/abm/src/app/manage-users/manage-users.component.spec.ts
@@ -1,14 +1,31 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { BrowserModule, By} from '@angular/platform-browser';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { MatTableModule, MatInputModule, MatSortModule, MatFormFieldModule, MatCheckboxModule, MatPaginatorModule, MatDialogModule} from '@angular/material';
+import { MatMenuModule, MatIconModule, MatToolbarModule} from '@angular/material';
+import { HttpModule } from '@angular/http';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { OrderModule } from 'ngx-order-pipe';
+
+import { DebugElement } from '@angular/core';
+import { UserService } from '../services/user.service';
+
 import { ManageUsersComponent } from './manage-users.component';
 
 describe('ManageUsersComponent', () => {
   let component: ManageUsersComponent;
   let fixture: ComponentFixture<ManageUsersComponent>;
+  let debugElement: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ManageUsersComponent ]
+      declarations: [ ManageUsersComponent ],
+      imports: [BrowserModule, BrowserAnimationsModule, HttpModule, MatToolbarModule, MatMenuModule, MatIconModule, MatTableModule, 
+        MatFormFieldModule, MatInputModule, MatCheckboxModule, MatSortModule, MatPaginatorModule,
+        OrderModule, MatDialogModule],
+      providers: [UserService, HttpClient, HttpHandler]
     })
     .compileComponents();
   }));
@@ -16,10 +33,39 @@ describe('ManageUsersComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ManageUsersComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  fit('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // fit('should check for isAllSelected function call', async(() => {
+  //   spyOn(component, 'isAllSelected');
+  //   const checkbox = fixture.debugElement.nativeElement.querySelector('mat-checkbox');
+  //   checkbox.click();
+  //   //fixture.whenStable().then(() => {
+  //   expect(component.isAllSelected).toHaveBeenCalled();
+  //   //});
+  // }));
+
+  fit('should check for apply filter keyup event', () => {
+    spyOn(component, 'applyFilter');
+    fixture.detectChanges();
+    const input = debugElement.query(By.css('#filter_data'));
+    const inputElement = input.nativeElement;
+    inputElement.dispatchEvent(new Event('keyup'));
+    expect(component.applyFilter).toHaveBeenCalled();
+    });
+
+    xit('should check for ooopenDialog function call', async(() => {
+      spyOn(component, 'deleteSelectedUsers');
+    fixture.detectChanges();
+    const input = debugElement.query(By.css('#button_alignR'));
+    const inputElement = input.nativeElement;
+    inputElement.click();
+    expect(component.deleteSelectedUsers).toHaveBeenCalled();
+    }));
+
 });
--- abm-frontend5/abm/src/app/services/user.service.spec.ts
+++ abm-frontend5/abm/src/app/services/user.service.spec.ts
@@ -1,15 +1,88 @@
-import { TestBed, inject } from '@angular/core/testing';
-
+import { TestBed, inject,fakeAsync, tick } from '@angular/core/testing';
+import {
+  JsonpModule,
+  Jsonp,
+  BaseRequestOptions,
+  Response,
+  ResponseOptions,
+  Http,
+  HttpModule
+} from "@angular/http";
+import { OrderPipe } from 'ngx-order-pipe';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import {MockBackend} from "@angular/http/testing";
 import { UserService } from './user.service';
+import { User } from '../models/user.model';
 
 describe('UserService', () => {
+
+  
+  let service: UserService;
+  let backend: MockBackend;
+  let model = new User('', '', '', '', '', '','', false);
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [UserService]
+      declarations: [ OrderPipe ],
+      imports: [HttpModule],
+      providers: [UserService, MockBackend,HttpClient, HttpHandler,
+        BaseRequestOptions,{
+          provide: Http,
+          useFactory: (backend, options) => new Http(backend, options),
+          deps: [MockBackend, BaseRequestOptions]
+        }]
     });
+
+    // Get the MockBackend
+    backend = TestBed.get(MockBackend);
+
+    // Returns a service with the MockBackend so we can test with dummy responses
+    service = TestBed.get(UserService);
+
   });
 
-  it('should be created', inject([UserService], (service: UserService) => {
+  fit('should be created', inject([UserService], (service: UserService) => {
     expect(service).toBeTruthy();
   }));
+
+  fit('search should return SearchItems', fakeAsync(() => {
+    
+    let response = {
+      "resultCount": 1,
+      "results": [
+        {
+          affiliation: "nvlfj",
+          approved: false,
+          email: "hh@hh.com",
+          firstname: "scvfacv",
+          lastname: "caedfea",
+          locked: false,
+          password: null,
+          role: "RegisteredUser",
+          token: null,
+          username: "cnoiwe"
+        }]
+    };
+
+    // When the request subscribes for results on a connection, return a fake response
+    backend.connections.subscribe(connection => {
+      connection.mockRespond(new Response(<ResponseOptions>{
+        body: JSON.stringify(response)
+      }));
+    });
+
+    // Perform a request and make sure we get the response we expect
+    service.getAllUsers(true).subscribe(response => {
+      this.usersList = JSON.stringify(response)
+    });
+    tick();
+
+    expect(service.getAllUsers.length).toBe(1);
+    // expect(service.results[0].artist).toBe("U2");
+    // expect(service.results[0].name).toBe("Beautiful Day");
+    // expect(service.results[0].thumbnail).toBe("image.jpg");
+    // expect(service.results[0].artistId).toBe(78500);
+  }));
 });
+
+
