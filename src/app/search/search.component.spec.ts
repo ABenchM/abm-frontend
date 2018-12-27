8b965792bad8be06a8fa1222cd66b09bab8a8735
mac24079 <mac24079@mail.uni-paderborn.de>
2018-12-28 00:28:51 +0100

    passing all test cases

--- src/app/search/search.component.spec.ts
+++ src/app/search/search.component.spec.ts
@@ -6,12 +6,12 @@ import { HttpModule } from '@angular/http';
 import { RouterTestingModule } from '@angular/router/testing';
 import { OrderModule } from 'ngx-order-pipe';
 import { ProjectCountPipe } from '../project-count.pipe';
-import { DataTableModule, PaginatorModule } from 'primeng/primeng';
-import { TableModule } from 'primeng/table';
 import { SearchService } from '../services/search.service';
 import { CollectionService } from '../services/collection.service';
 import { FileSizePipe } from '../shared/file-size.pipe';
 import { SiteNamePipe } from '../shared/site-name.pipe';
+import { MatTableModule, MatInputModule, MatSortModule, MatFormFieldModule, MatCheckboxModule} from '@angular/material';
+import { MatMenuModule, MatIconModule, MatToolbarModule, MatPaginatorModule, MatDialogModule} from '@angular/material';
 
 describe('SearchComponent', () => {
   let component: SearchComponent;
@@ -20,8 +20,8 @@ describe('SearchComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ SearchComponent , ProjectCountPipe, FileSizePipe, SiteNamePipe],
-      imports: [FormsModule, ReactiveFormsModule, HttpModule, RouterTestingModule, OrderModule, DataTableModule
-        , PaginatorModule, TableModule],
+      imports: [FormsModule, ReactiveFormsModule, HttpModule, RouterTestingModule, OrderModule,
+         MatMenuModule, MatIconModule, MatFormFieldModule, MatDialogModule, MatPaginatorModule, MatSortModule, MatTableModule, MatInputModule, MatCheckboxModule],
         providers: [SearchService, CollectionService]
     })
     .compileComponents();
