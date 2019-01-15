c05fd418e49499d36f98c54bba9b379df5af0f04
Minal <minal0110@gmail.com>
2019-01-15 20:10:25 +0100

    updated specs file

--- src/app/admin-pending-req/admin-pending-req.component.spec.ts
+++ src/app/admin-pending-req/admin-pending-req.component.spec.ts
@@ -1,52 +1,39 @@
+import { AdminPendingReqComponent } from './admin-pending-req.component';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-  import { BrowserModule, By} from '@angular/platform-browser';
-  import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
-  import { MatTableModule, MatInputModule, MatSortModule, MatFormFieldModule, MatDialogRef} from '@angular/material';
-  import {MatCheckboxModule, MatPaginatorModule, MatButtonModule} from '@angular/material';
-  import { MatMenuModule, MatIconModule, MatToolbarModule, MatTableDataSource, MAT_DIALOG_DATA} from '@angular/material';
-  import { AdminPendingReqComponent } from './admin-pending-req.component';
-  import { DebugElement } from '@angular/core';
-  import { HttpModule } from '@angular/http';
-  import { HttpClient, HttpHandler } from '@angular/common/http';
-  import { OrderModule } from 'ngx-order-pipe';
-  import { UserService } from '../services/user.service';
-  import { DialogBoxComponent } from '../dialog-box/dialog-box.component';
-  import { MatDialogModule} from '@angular/material/dialog';
-  import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BrowserModule, By} from '@angular/platform-browser';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatTableModule, MatInputModule, MatSortModule, MatFormFieldModule, MatCheckboxModule} from '@angular/material';
+import { MatMenuModule, MatIconModule, MatToolbarModule, MatPaginatorModule, MatDialogModule} from '@angular/material';
+import { HttpModule } from '@angular/http';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { OrderModule } from 'ngx-order-pipe';
+import { DebugElement } from '@angular/core';
+import { UserService } from '../services/user.service';
 
-  describe('AdminPendingReqComponent', () => {
-   let component: AdminPendingReqComponent;
-   let fixture: ComponentFixture<AdminPendingReqComponent>;
-   let debugElement: DebugElement;
+describe('AdminPendingReqComponent', () => {
+  let component: AdminPendingReqComponent;
+  let fixture: ComponentFixture<AdminPendingReqComponent>;
+  let debugElement: DebugElement;
 
-   beforeEach(async(() => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
-    declarations: [ AdminPendingReqComponent, DialogBoxComponent ],
-    schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
-    imports: [BrowserModule, BrowserAnimationsModule, HttpModule, MatToolbarModule, MatMenuModule,
-    MatIconModule, MatTableModule, MatInputModule,
-    MatFormFieldModule, MatInputModule, MatCheckboxModule, MatSortModule,
-    MatPaginatorModule, OrderModule, MatDialogModule, MatButtonModule],
-    providers:  [UserService, HttpClient, HttpHandler, {
-      provide: MatDialogRef, useValue: {}
-      }, {
-      provide: MAT_DIALOG_DATA, useValue: {}
-      },
-      {
-      provide: MatTableDataSource, useValue: {}
-      }]
-      })
-      .compileComponents();
-      }));
+      declarations: [ AdminPendingReqComponent ],
+      imports: [BrowserModule, BrowserAnimationsModule, HttpModule, MatToolbarModule, MatMenuModule, MatIconModule, MatTableModule,
+        MatFormFieldModule, MatInputModule, MatCheckboxModule, MatSortModule, MatPaginatorModule,
+        OrderModule, MatDialogModule],
+      providers: [UserService, HttpClient, HttpHandler]
+    })
+    .compileComponents();
+  }));
 
-   beforeEach(() => {
-   fixture = TestBed.createComponent(AdminPendingReqComponent);
-   component = fixture.componentInstance;
-   debugElement = fixture.debugElement;
-   fixture.detectChanges();
-   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AdminPendingReqComponent);
+    component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
+    fixture.detectChanges();
+    });
 
-   fit('should create', () => {
+  fit('should create', () => {
     expect(component).toBeTruthy();
   });
 
@@ -55,21 +42,6 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
     const de = fixture.debugElement.nativeElement;
     expect(de.querySelector('h2').textContent).toContain('Pending Request');
   });
-
-  fit('should check for apply filter keyup event', () => {
-    spyOn(component, 'applyFilter');
-    fixture.detectChanges();
-    const input = debugElement.query(By.css('#filter_data'));
-    const inputElement = input.nativeElement;
-    inputElement.dispatchEvent(new Event('keyup'));
-    expect(component.applyFilter).toHaveBeenCalled();
-    });
-
-  fit('should call masterToggle on change checkbox', () => {
-    const checkbox = spyOn(component, 'masterToggle');
-    fixture.debugElement.query(By.css('#CheckBox')).triggerEventHandler('change', {});
-    expect(checkbox).toHaveBeenCalled();
-    });
 
   fit('should invoke approveSelectedUsers button', () => {
     const approveSelectedUsers = spyOn(component, 'approveSelectedUsers');
@@ -82,4 +54,13 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
   fixture.debugElement.query(By.css('#buttonL')).triggerEventHandler('click', null);
   expect(rejectSelectedUsers).toHaveBeenCalled();
   });
-  });
+
+  fit('should check for apply filter keyup event', () => {
+    spyOn(component, 'applyFilter');
+    fixture.detectChanges();
+    const input = debugElement.query(By.css('#filter_data'));
+    const inputElement = input.nativeElement;
+    inputElement.dispatchEvent(new Event('keyup'));
+    expect(component.applyFilter).toHaveBeenCalled();
+    });
+});
