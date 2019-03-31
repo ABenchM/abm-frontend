ea4f01f4f5a9eca91c40ead24099b24efb8a4d6f
mac24079 <mac24079@mail.uni-paderborn.de>
2019-03-31 16:28:04 +0200

    fixed paginator issue on public collections page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -35,7 +35,8 @@ export class HomeComponent implements OnInit {
   expandedElement: any;
   versions: any[] = [{}];
   pinnedversions: any[] = [{}];
-  @ViewChildren(MatPaginator) paginator = new QueryList<MatPaginator>();
+  @ViewChildren('pinPaginator') pinPaginator = new QueryList<MatPaginator>();
+  @ViewChildren('pubPaginator') pubPaginator = new QueryList<MatPaginator>();
   @ViewChildren(MatSort) sort = new QueryList<MatSort>();
   isExpansionDetailRow = (row: any) => row.hasOwnProperty('detailRow');
 
@@ -50,26 +51,26 @@ export class HomeComponent implements OnInit {
         this.publicCollections = this.orderPipe.transform(response.json());
         let j = 0;
         while (j < this.publicCollections.length) {
-        this.versions = this.publicCollections[j].versions;
-        let i = 0;
-        while (i < this.versions.length) {
-          // console.log('Status and id ' + response.json()[0].versions[i].privateStatus + ' ' + response.json()[0].versions[i].id);
-          if (this.versions[i].privateStatus === true) {
-            console.log('Deleting version ' + this.versions[i].id);
-            this.versions.splice(i, 1);
+          this.versions = this.publicCollections[j].versions;
+          let i = 0;
+          while (i < this.versions.length) {
+            // console.log('Status and id ' + response.json()[0].versions[i].privateStatus + ' ' + response.json()[0].versions[i].id);
+            if (this.versions[i].privateStatus === true) {
+              console.log('Deleting version ' + this.versions[i].id);
+              this.versions.splice(i, 1);
 
+            } else {
+              i = i + 1;
+            }
+          }
+          if (this.versions.length <= 0) {
+            this.publicCollections.splice(j, 1);
           } else {
-            i = i + 1;
+            j = j + 1;
           }
         }
-        if (this.versions.length <= 0) {
-          this.publicCollections.splice(j, 1);
-        } else {
-          j = j + 1;
-        }
-      }
         this.dataSourcePub.data = this.publicCollections;
-        setTimeout(() => this.dataSourcePub.paginator = this.paginator.toArray()[1]);
+        setTimeout(() => this.dataSourcePub.paginator = this.pubPaginator.toArray()[0]);
         setTimeout(() => this.dataSourcePub.sort = this.sort.toArray()[1]);
         if (this.loggedInStatus()) {
           this.loadPinned();
@@ -90,26 +91,26 @@ export class HomeComponent implements OnInit {
         this.pinned = this.orderPipe.transform(response.json());
         let j = 0;
         while (j < this.pinned.length) {
-        this.pinnedversions = this.pinned[j].versions;
-        let i = 0;
-        while (i < this.pinnedversions.length) {
-          // console.log('Status and id ' + response.json()[0].versions[i].privateStatus + ' ' + response.json()[0].versions[i].id);
-          if (this.pinnedversions[i].privateStatus === true) {
-            console.log('Deleting version ' + this.pinnedversions[i].id);
-            this.pinnedversions.splice(i, 1);
+          this.pinnedversions = this.pinned[j].versions;
+          let i = 0;
+          while (i < this.pinnedversions.length) {
+            // console.log('Status and id ' + response.json()[0].versions[i].privateStatus + ' ' + response.json()[0].versions[i].id);
+            if (this.pinnedversions[i].privateStatus === true) {
+              console.log('Deleting version ' + this.pinnedversions[i].id);
+              this.pinnedversions.splice(i, 1);
 
+            } else {
+              i = i + 1;
+            }
+          }
+          if (this.pinnedversions.length <= 0) {
+            this.pinned.splice(j, 1);
           } else {
-            i = i + 1;
+            j = j + 1;
           }
         }
-        if (this.pinnedversions.length <= 0) {
-          this.pinned.splice(j, 1);
-        } else {
-          j = j + 1;
-        }
-      }
         this.dataSourcePin.data = this.pinned;
-        setTimeout(() => this.dataSourcePin.paginator = this.paginator.toArray()[0]);
+        setTimeout(() => this.dataSourcePin.paginator = this.pinPaginator.toArray()[0]);
         setTimeout(() => this.dataSourcePin.sort = this.sort.toArray()[0]);
       }
 
@@ -119,18 +120,18 @@ export class HomeComponent implements OnInit {
   }
 
   openVersion(versions, versionID) {
-     let i = 0;
-     let index;
-     while (i < versions.length) {
-       if (versions[i].id === versionID) {
-         index = i;
-       }
-       i++;
-     }
-     if (index != null) {
-       index++;
+    let i = 0;
+    let index;
+    while (i < versions.length) {
+      if (versions[i].id === versionID) {
+        index = i;
+      }
+      i++;
+    }
+    if (index != null) {
+      index++;
       this.router.navigateByUrl('/view/' + versions[0].collectionId + '/' + index);
-     }
+    }
   }
 
   checkPinned(item) {
@@ -191,11 +192,11 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit() {
-  if (this.loggedInStatus()) {
-    this.displayedColumns = ['name', 'description', 'creation_date', 'id', 'pin', 'versions' ];
-  } else {
-    this.displayedColumns = ['name', 'description', 'creation_date', 'id', 'versions' ];
-  }
+    if (this.loggedInStatus()) {
+      this.displayedColumns = ['name', 'description', 'creation_date', 'id', 'pin', 'versions'];
+    } else {
+      this.displayedColumns = ['name', 'description', 'creation_date', 'id', 'versions'];
+    }
     this.loadPublicCollections();
   }
 
